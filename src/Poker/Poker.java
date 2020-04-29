a672e2e75132fa8238b289f3f1e4bc70bdf0ad7e
Timothy Hart <timothyhart6@gmail.com>
2020-04-28 22:07:54 -0400

    Adding IO to Poker Class. Still in rough shape.

--- src/Poker/Poker.java
+++ src/Poker/Poker.java
@@ -1,4 +1,25 @@
 package Poker;
 
-public class Poker {
+import java.util.Scanner;
+
+public  class Poker {
+
+    Player player;
+    static String playerName;
+    private static int playerChipCount;
+
+
+    private static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+        playerName = reader.next();
+        System.out.println("Hello " + playerName + ". How many chips are you buying in for?");
+        playerChipCount = reader.nextInt();
+        System.out.println("You're buying in for " + playerChipCount + " chips.");
+        Player player = new Player(playerName, playerChipCount);
+        
+        reader.close();
+    }
+
 }
